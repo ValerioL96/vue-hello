521ddd64ac72317a2f089c5d7391853677764bd5
ValerioL96 <v.lembo1396@gmail.com>
2024-05-29 09:31:42 +0200

    fix: Deleted <script> in JavaScript

--- js/scrip.js
+++ js/scrip.js
@@ -1,17 +1,13 @@
-//2) creo un file Javascript:
-  
-   // - nel file javascript creo una variabile
-        <script>
-          const { createApp } = Vue
 
-                createApp({
-                    data() {
-                        return {
-                            message : 'Hello Vue!'
-                        }
-                    }
-                
-    //- successivamente sarà montata al div in html      
 
-                }).mount('#app')
-        </script>
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+
